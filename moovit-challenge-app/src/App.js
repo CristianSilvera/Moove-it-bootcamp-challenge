8916b21b63d441aabadab5a5256572e8560555ec
kiddopro <ms.prgramador@gmail.com>
2021-06-11 23:40:12 -0300

    Se agregaron nuevos features a las notas

--- moovit-challenge-app/src/App.js
+++ moovit-challenge-app/src/App.js
@@ -1,43 +1,42 @@
 //import logo from './logo.svg';
 //import './App.css';
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
-import NotesList from './components/NotesList';
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import NotesList from "./components/NotesList";
 
 const App = () => {
   const [notes] = useState([
-
     {
       id: nanoid(),
       text: "This is my first note!",
-      date: "30/04/2021"
+      date: "30/04/2021",
     },
     {
       id: nanoid(),
       text: "This is my second note!",
-      date: "25/04/2021"
+      date: "25/04/2021",
     },
     {
       id: nanoid(),
       text: "This is my third note!",
-      date: "20/04/2021"
+      date: "20/04/2021",
     },
     {
       id: nanoid(),
-      text: "This is my four note!",
-      date: "19/04/2021"
-    }
-]);
+      text: "This is my forth note!",
+      date: "19/04/2021",
+    },
+  ]);
+
+  const addNote = (text) => {
+    console.log(text);
+  };
+
   return (
-
     <div className="container">
-
-      <NotesList notes={notes}/>
-
+      <NotesList notes={notes} handleAddNote={addNote} />
     </div>
-
   );
-
 };
 
 export default App;
--- moovit-challenge-app/src/components/NotesList.js
+++ moovit-challenge-app/src/components/NotesList.js
@@ -1,22 +1,16 @@
 //Esto va a funcionar como un cotenedor de las notas y ayuda a ubicar mejor los layouts, darle estilos, etc.
-import Note from './Note';
+import AddNote from "./AddNote";
+import Note from "./Note";
 
-const NotesList = ({
-	notes
-
-}) => {
-	return (
-		<div className='notes-list'>
-			{notes.map((note) => (
-				<Note
-					id={note.id}
-					text={note.text}
-					date={note.date}
-					
-				/>
-			))}
-		</div>
-	);
+const NotesList = ({ notes, handleAddNote }) => {
+  return (
+    <div className="notes-list">
+      {notes.map((note) => (
+        <Note id={note.id} text={note.text} date={note.date} />
+      ))}
+      <AddNote handleAddNote={handleAddNote} />
+    </div>
+  );
 };
 
 export default NotesList;
